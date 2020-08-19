4ce8add2012b3264b78648af1e52a58adbea851c
ofir262 <69913261+ofir262@users.noreply.github.com>
2020-08-19 11:50:55 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/**************************************************************************************
+* Hello.java
+***************************************************************************************
+*Ofir Abramson
+*8/19/2020
+*CMSC 255 Section 004
+**************************************************************************************/
+
+public class Hello {
+	public static void main(String[] args){
+		
+		System.out.println("< Ofir Abramson>");
+		System.out.println("<Java Hello>");
+		System.out.println("<Display information>");
+		System.out.println("<8/19/2020>");
+		System.out.println("<CMSC 255 004>");
+		System.out.println();
+	}
+}
