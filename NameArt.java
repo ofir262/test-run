51db4738bff86280cc4fa3534544f5eeceeeb9d9
ofir262 <69913261+ofir262@users.noreply.github.com>
2020-08-27 03:51:42 -0400

    Add files via upload

---
+++ NameArt.java
@@ -0,0 +1,29 @@
+/*********************************************************************************
+*Nameart.java
+**********************************************************************************
+*Ofir Abramson
+*Project1; Name in code
+*This program will write my name -OFIR- next to my attempt at turtle
+*8/26/2020
+*CMSC 255 004
+*********************************************************************************/
+public class NameArt
+{
+	public static void main(String [] args)
+	{  
+		System.out.println("                                                                                                   ");
+		System.out.println("        OOOOOO     FFFFFFFFFF  IIIIIIII  RRRRRRRR                                                  ");
+		System.out.println("      OOOOOOOOOO   FFFFFFFFFF   IIIIII   RRRRRRRRR           .. .. .. ....                         ");
+		System.out.println("     OOO      OOO  FF             II     RR     RR         ..  ---------  ..                       ");
+		System.out.println("     OO        OO  FF             II     RR     RR       ..                 ..                     ");
+		System.out.println("     OO  $  $  OO  FF             II     RR    RR       ..  --------------    ..                   ");
+		System.out.println("     OO        OO  FFFFFF         II     RRRRRRR       ..                      ..   .. .. ..       ");
+		System.out.println("     OO  |  |  OO  FFFFF          II     RRRRRRR      ..  -------------------   .. .         .     ");
+		System.out.println("     OO   --   OO  FF             II     RR    RR   . .  .                        .   o    o  .    ");
+		System.out.println("     OOO      OOO  FF             II     RR     RR   .   . -----------------   .              .    ");
+		System.out.println("      OOOOOOOOOO   FF           IIIIII   RR      RR   .    .. .. .. .. .. .. .. .       -    .     ");
+		System.out.println("        OOOOOO     FF          IIIIIIII  RR       RR   . .                     .   .. .. ...       ");
+		System.out.println("                                                          .   . . . . . . .   .                    ");
+		System.out.println("                                                           ...             ...                     ");
+	}
+}  
